1621aa657b99d3038ce72f80877ef70ef98e2352
Andre Fonseca <afbfonseca@gmail.com>
2016-04-11 11:07:01 +0100

    Actor class and arena update

--- Exercises/src/Arena/Arena.java
+++ Exercises/src/Arena/Arena.java
@@ -3,6 +3,7 @@ package Arena;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -12,21 +13,31 @@ public class Arena extends JPanel {
 	private static final int MAX_Y = 400;
 	private static final int POS_INIT_X = 200;
 	private static final int POS_INIT_Y = 200;
+	private static final int NUM_ACTORS = 3;
+	
+	private Actor[] actors = new Actor[NUM_ACTORS];
+	
+	public Arena() {
+		Random r = new Random();
+		
+		for (int i = 0; i < actors.length; i++) {
+			actors[i] = new Actor();
+			actors[i].setSize(100);
+			actors[i].setX(r.nextInt(MAX_X - actors[i].getSize()));
+			actors[i].setY(r.nextInt(MAX_Y - actors[i].getSize()));
+			actors[i].setColor(new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+		}
+	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D draw = (Graphics2D) g;
 		
-		Color color = new Color(150, 150, 150);
-		draw.setColor(color);
-		
-		draw.fillRect(10, 10, 100, 100);
-		
-		color = new Color(255, 0, 0);
-		draw.setColor(color);
-		
-		draw.fillRect(100, 100, 50, 50);
+		for (int i = 0; i < actors.length; i++) {
+			draw.setColor(actors[i].getColor());
+			draw.fillRect(actors[i].getX(), actors[i].getY(), actors[i].getSize(), actors[i].getSize());
+		}
 	}
 	
 	public static void main(String[] args) {
